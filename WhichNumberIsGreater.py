b01d1bbd530fd44daf493c8fcf30b125285ee88e
Vitor Marques <89469589+VitorMarques96@users.noreply.github.com>
2023-04-10 10:57:25 -0300

    Delete WhichNumberIsGreater.py

--- WhichNumberIsGreater.py
+++
@@ -1,7 +0,0 @@
-primeiro_valor = input('Digite o primeiro valor: ')
-segundo_valor = input('Digite o segundo valor: ')
-
-if primeiro_valor > segundo_valor:
-    print(f'{primeiro_valor=} é maior do que {segundo_valor=}')
-else:
-    print(f'{segundo_valor=} é maior do que {primeiro_valor=}')
