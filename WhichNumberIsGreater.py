8e8cc691d208a76ec75d95d548f4d7130db4d650
Vitor Marques <89469589+VitorMarques96@users.noreply.github.com>
2023-04-10 10:57:36 -0300

    Add files via upload

---
+++ WhichNumberIsGreater.py
@@ -0,0 +1,7 @@
+first_number = input('Enter the first number: ')
+second_number = input('Enter the second number: ')
+
+if first_number > second_number:
+    print(f"{first_number=} it's greater than {second_number=}")
+else:
+    print(f"{second_number=} it's greater than {first_number=}")
