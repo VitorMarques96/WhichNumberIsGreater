0fd23219858b9959c7e751a9259b636b0fec49a8
Vitor Marques <89469589+VitorMarques96@users.noreply.github.com>
2023-04-10 11:08:48 -0300

    Update WhichNumberIsGreater.py

--- WhichNumberIsGreater.py
+++ WhichNumberIsGreater.py
@@ -3,5 +3,7 @@ second_number = input('Enter the second number: ')
 
 if first_number > second_number:
     print(f"{first_number=} it's greater than {second_number=}")
+elif first_number == second_number:
+    print(f"{first_number=} is equal to the {second_number=}")    
 else:
     print(f"{second_number=} it's greater than {first_number=}")
